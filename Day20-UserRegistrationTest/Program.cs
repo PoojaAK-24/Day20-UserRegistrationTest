8e97c57ba4d6fb97e3ef5c4b29cb542a5db2f3b1
PoojaAK-24 <85796760+PoojaAK-24@users.noreply.github.com>
2021-07-28 12:27:20 +0530

    Pooja Added:Validate User's Email Id

--- Day20-UserRegistrationTest/Program.cs
+++ Day20-UserRegistrationTest/Program.cs
@@ -7,6 +7,7 @@ namespace Day20_UserRegistrationTest
     {
         public static string REGEX_PATTERN = "^[A-Z]{1}[a-zA-Z]{2,}$";
         public static Program p = new Program();
+        
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome_To_UserRegistration_RegeEx");
@@ -16,17 +17,26 @@ namespace Day20_UserRegistrationTest
             Console.Write("Enter Last Name: ");
             string LastName = Console.ReadLine();
             Console.WriteLine(p.LastNameVal(LastName));
+            Console.Write("Enter E-mail : ");
+            string Email = Console.ReadLine();
+            Console.WriteLine(p.EmailValidation(Email));
         }
         public bool FirstNameVal(string first_name)
         {
             Regex rg = new Regex(REGEX_PATTERN);
             return rg.IsMatch(first_name); ;
         }
-
         public bool LastNameVal(string last_name)
         {
             Regex rg = new Regex(REGEX_PATTERN);
             bool validate = rg.IsMatch(last_name);
+            return validate;
+        }
+        public bool EmailValidation(string email)
+        {
+            string EMAIL_PATTERN = "^[a-zA-Z0-9]+[.(a-zA-Z0-9)]*(\\@)[a-zA-Z0-9]+(\\.)[a-z]{2,3}[.(a-z)]*$";
+            Regex rg = new Regex(EMAIL_PATTERN);
+            bool validate = rg.IsMatch(email);
             return validate;
         }
     }
--- UserRegistrationTest/UnitTest1.cs
+++ UserRegistrationTest/UnitTest1.cs
@@ -18,12 +18,18 @@ namespace UserRegistrationTest
             bool result = program.FirstNameVal(name);
             Assert.AreEqual(true, result);
         }
-
         [TestMethod]
         public void LastNameTest()
         {
             string name = "Reddy";
             bool result = program.LastNameVal(name);
+            Assert.AreEqual(true, result);
+        }
+        [TestMethod]
+        public void EmailTest()
+        {
+            string email = "Pooja.reddy@gmail.com.in";
+            bool result = program.EmailValidation(email);
             Assert.AreEqual(true, result);
         }
     }
