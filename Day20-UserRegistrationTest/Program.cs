aed98be9b0541904bd2c03aec722cd27b4f1aa04
PoojaAK-24 <85796760+PoojaAK-24@users.noreply.github.com>
2021-07-28 13:01:53 +0530

    Pooja Added:Validate User's Password,Should Contain Atleast 1 Uppercase

--- Day20-UserRegistrationTest/Program.cs
+++ Day20-UserRegistrationTest/Program.cs
@@ -7,7 +7,8 @@ namespace Day20_UserRegistrationTest
     {
         public static string REGEX_PATTERN = "^[A-Z]{1}[a-zA-Z]{2,}$";
         public static Program p = new Program();
-       static void Main(string[] args)
+       
+        static void Main(string[] args)
         {
             Console.WriteLine("Welcome_To_UserRegistration_RegeEx");
             Console.Write("Enter First Name: ");
